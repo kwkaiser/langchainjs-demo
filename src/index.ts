5ec14ccbb2b5a20e7184874228bfb8c01d3d7508
kwkaiser <karl@kwkaiser.io>
2025-05-30 16:43:35 -0400

    Demo

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-// Using ES module syntax
-export const message = "Hello, World!";
-console.log(message);
+import { z } from "zod";
+import { ChatVertexAI } from "@langchain/google-vertexai";
+import { authOptions } from "./auth";
+
+const validSchema = z.object({
+  greeterName: z.string().nullable(),
+});
+
+const invalidSchema = {
+  type: "object",
+  properties: {
+    greeterName: {
+      type: ["string", "null"],
+    },
+  },
+  required: ["greeterName"],
+};
+
+(async () => {
+  const model = new ChatVertexAI({
+    modelName: "gemini-2.0-flash-001",
+    temperature: 0,
+    authOptions,
+  });
+
+  const result = await model
+    .withStructuredOutput(validSchema)
+    .invoke("Hello, world from: kwkaiser");
+  console.log("From zod schema call", result);
+
+  try {
+    await model
+      .withStructuredOutput(invalidSchema)
+      .invoke("Hello, world from: kwkaiser");
+  } catch (err) {
+    console.log("Encountered error with JSON schema\n", err);
+  }
+})().then(() => process.exit(0));
